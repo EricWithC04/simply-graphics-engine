70a3f75d4b3be2dc9fc4f940ec898b459eaa6ba7
EricWithC04 <negativegame117@gmail.com>
2025-06-04 10:24:27 -0300

    feat: logic for multiple entities

--- src/main.ts
+++ src/main.ts
@@ -11,18 +11,31 @@ const player = new Entity()
 const position = new PositionComponent(100, 100)
 const renderer = new RenderRectComponent(position, 50, 50, "red")
 
+const player2 = new Entity()
+const position2 = new PositionComponent(100, 300)
+const renderer2 = new RenderRectComponent(position2, 50, 50, "blue")
+
 player
     .addComponent(position)
     .addComponent(renderer)
 
+player2
+    .addComponent(position2)
+    .addComponent(renderer2)
+
+const entities: Entity[] = [player, player2]
+
 const update = (delta: number) => {
-    position.update(delta);
+    entities.forEach((e) => {
+        e.update(delta);
+    })
 }
 
 const render = () => {
     canvas.clear();
 
     player.render(ctx);
+    player2.render(ctx)
 }
 
 const loop = new GameLoop(update, render)
