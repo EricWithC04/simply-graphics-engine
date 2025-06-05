4a4ad43079f62089d12b2dcb0a827a9be5606870
EricWithC04 <negativegame117@gmail.com>
2025-06-05 20:25:14 -0300

    feat: image loader

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { Canvas } from "./core/Canvas";
 import { GameLoop } from "./core/GameLoop";
 import { Entity } from "./entities";
+
 import { PositionComponent } from "./entities/components/PositionComponent";
 import { RenderRectComponent } from "./entities/components/RenderRectComponent";
 import { VelocityComponent } from "./entities/components/VelocityComponent";
 import { KeyboardInputComponent } from "./entities/components/KeyboardInputComponent";
 import { ColliderComponent } from "./entities/components/ColliderComponent";
+import { SpriteRenderComponent } from "./entities/components/SpriteRenderComponent";
+
 import { CollisionSystem } from "./systems/CollisionSystem";
+
+import { AssetLoader } from "./assets/AssetLoader";
 
 const canvas = new Canvas(800, 600)
 const ctx = canvas.getContext();
@@ -15,7 +20,7 @@ const collisionSystem = new CollisionSystem()
 
 const player = new Entity()
 const position = new PositionComponent(100, 100)
-const renderer = new RenderRectComponent(position, 50, 50, "red")
+// const renderer = new RenderRectComponent(position, 50, 50, "red")
 const velocity = new VelocityComponent(position)
 const input = new KeyboardInputComponent(velocity)
 
@@ -27,13 +32,20 @@ const obstaclePosition = new PositionComponent(100, 300)
 const obstacleRenderer = new RenderRectComponent(obstaclePosition, 50, 50, "blue")
 const obstacleCollider = new ColliderComponent(obstaclePosition, 50, 50)
 
-collisionSystem.addCollider(obstacleCollider)
-player
-    .addComponent(position)
-    .addComponent(velocity)
-    .addComponent(input)
-    .addComponent(renderer)
-    .addComponent(playerCollider)
+collisionSystem.addCollider(obstacleCollider);
+
+(async () => {
+    const playerImage = await AssetLoader.loadImage("./tests/character_test_image.png")
+
+    const renderer = new SpriteRenderComponent(position, playerImage, 50, 50)
+
+    player
+        .addComponent(position)
+        .addComponent(velocity)
+        .addComponent(input)
+        .addComponent(renderer)
+        .addComponent(playerCollider)
+})()
 
 obstacle
     .addComponent(obstaclePosition)
