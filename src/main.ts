ed52e5266113fdd592414cd9eca334ee57262d21
EricWithC04 <negativegame117@gmail.com>
2025-06-14 11:04:58 -0300

    feat: tool of fps counter

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,17 @@
 import { Canvas } from "./core/Canvas";
 import { GameLoop } from "./core/GameLoop";
-import { Entity } from "./entities";
+// import { Entity } from "./entities";
 
 import { SceneManager } from "./scenes/SceneManager";
 import { GameScene } from "./scenes/GameScene";
 
-import { PositionComponent } from "./entities/components/PositionComponent";
-import { VelocityComponent } from "./entities/components/VelocityComponent";
-import { KeyboardInputComponent } from "./entities/components/KeyboardInputComponent";
-import { ColliderComponent } from "./entities/components/ColliderComponent";
-import { SpriteRenderComponent } from "./entities/components/SpriteRenderComponent";
+// import { PositionComponent } from "./entities/components/PositionComponent";
+// import { VelocityComponent } from "./entities/components/VelocityComponent";
+// import { KeyboardInputComponent } from "./entities/components/KeyboardInputComponent";
+// import { ColliderComponent } from "./entities/components/ColliderComponent";
+// import { SpriteRenderComponent } from "./entities/components/SpriteRenderComponent";
 
-import { CollisionSystem } from "./systems/CollisionSystem";
+// import { CollisionSystem } from "./systems/CollisionSystem";
 
 import { AssetLoader } from "./assets/AssetLoader";
 
@@ -19,64 +19,42 @@ const canvas = new Canvas(800, 600)
 const ctx = canvas.getContext();
 const sceneManager = new SceneManager()
 
-const collisionSystem = new CollisionSystem()
+// const collisionSystem = new CollisionSystem()
 
-const player = new Entity()
-const position = new PositionComponent(100, 100)
-// const renderer = new RenderRectComponent(position, 50, 50, "red")
-const velocity = new VelocityComponent(position)
-const input = new KeyboardInputComponent(velocity)
+// const player = new Entity()
+// const position = new PositionComponent(100, 100)
+// const velocity = new VelocityComponent(position)
+// const input = new KeyboardInputComponent(velocity)
 
-const playerCollider = new ColliderComponent(position, 50, 50)
-collisionSystem.addCollider(playerCollider);
+// const playerCollider = new ColliderComponent(position, 50, 50)
+// collisionSystem.addCollider(playerCollider);
 
-// const obstacle = new Entity()
-// const obstaclePosition = new PositionComponent(100, 300)
-// const obstacleRenderer = new RenderRectComponent(obstaclePosition, 50, 50, "blue")
-// const obstacleCollider = new ColliderComponent(obstaclePosition, 50, 50)
+// (async () => {
+//     const playerImage = await AssetLoader.loadImage("./tests/character_test_image.png")
 
-// collisionSystem.addCollider(obstacleCollider);
+//     const renderer = new SpriteRenderComponent(position, playerImage, 50, 50)
 
-(async () => {
-    const playerImage = await AssetLoader.loadImage("./tests/character_test_image.png")
-
-    const renderer = new SpriteRenderComponent(position, playerImage, 50, 50)
-
-    player
-        .addComponent(position)
-        .addComponent(velocity)
-        .addComponent(input)
-        .addComponent(renderer)
-        .addComponent(playerCollider)
-})()
-
-// obstacle
-//     .addComponent(obstaclePosition)
-//     .addComponent(obstacleRenderer)
-//     .addComponent(obstacleCollider)
-
-// const entities: Entity[] = [player, obstacle]
+//     player
+//         .addComponent(position)
+//         .addComponent(velocity)
+//         .addComponent(input)
+//         .addComponent(renderer)
+//         .addComponent(playerCollider)
+// })()
 
 const update = (delta: number) => {
-    // entities.forEach((e) => {
-    //     e.update(delta);
-    // })
     sceneManager.update(delta)
 }
 
 const render = () => {
     canvas.clear();
 
-    // entities.forEach((e) => {
-    //     e.render(ctx)
-    // })
     sceneManager.render(ctx)
 
-    collisionSystem.update()
+    // collisionSystem.update()
 };
 
 (async () => {
-    // const playerImage = await AssetLoader.loadImage("./tests/character_test_image.png")
     const playerSpriteSheet = await AssetLoader.loadImage("./tests/test-sprite-sheet.jpg")
     const gameScene = new GameScene(playerSpriteSheet)
 
---
+++ src/tools/FPSCounter.ts
@@ -0,0 +1,22 @@
+export class FPSCounter {
+    private frameCount = 0;
+    private lastTime = performance.now();
+    private fps = 0;
+
+    update() {
+        this.frameCount++;
+        const now = performance.now();
+
+        if (now - this.lastTime >= 1000) {
+            this.fps = this.frameCount;
+            this.frameCount = 0;
+            this.lastTime = now;
+        }
+    }
+
+    render(ctx: CanvasRenderingContext2D) {
+        ctx.fillStyle = "white";
+        ctx.font = "14px monospace";
+        ctx.fillText(`FPS: ${this.fps}`, 10, 20)
+    }
+}
